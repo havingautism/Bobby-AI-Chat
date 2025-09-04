1d4def97a0dbed022655041b8a52459e7062d2b3
havingautism <havingautism@outlook.com>
2025-09-04 23:09:08 +0800

    更新聊天界面和模型选择器的图标，调整侧边栏的样式以改善用户体验，同时在设置文件中添加新的读取路径。

--- src/components/ChatInterface.js
+++ src/components/ChatInterface.js
@@ -688,10 +688,10 @@ const ChatInterface = ({
               }}
               title={t("timeline", currentLanguage)}
             >
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <circle cx="12" cy="12" r="10"/>
-                <polyline points="12,6 12,12 16,14"/>
-              </svg>
+             
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 24 24" >{/* Icon from Google Material Icons by Material Design Authors - https://github.com/material-icons/material-icons/blob/master/LICENSE */}<path fill="currentColor" d="M6 15h6v2H6zm6-8h6v2h-6zm-3 4h6v2H9z" /><path fill="currentColor" d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m0 16H5V5h14z" /></svg>
+  
             </button>
           )}
         </div>
